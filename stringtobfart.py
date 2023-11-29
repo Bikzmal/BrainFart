3695b724d3869b30402fe47233213eb09b1fb539
ympl <89935769+Bikzmal@users.noreply.github.com>
2023-11-29 19:26:10 +0100

    Create stringtobfart.py

---
+++ stringtobfart.py
@@ -0,0 +1,32 @@
+import sys
+
+if len(sys.argv) < 2:
+	print("Usage: python3 stringtobfart.py <string>")
+	sys.exit(1)
+
+string = " ".join(sys.argv[1:])
+
+code = ""
+
+for c in string:
+	x = ord(c)
+	y = 0
+	while y != x:
+		if (y+10) * 10 <= x:
+			code += "A"
+			y += 10
+		elif (y+1) * 10 <= x:
+			code += "a"
+			y += 1
+		elif y * 10 <= x:
+			code += "m"
+			y *= 10
+		elif y + 10 <= x:
+			code += "A"
+			y += 10
+		elif y + 1 <= x:
+			code += "a"
+			y += 1
+	code += "oc"
+
+print(code)
